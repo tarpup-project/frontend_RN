009cfcf45594ad50cbd71bb2b724b94652df9e6c
vector-10 <duzieblaise10@gmail.com>
2025-12-02 20:50:26 +0100

    chore: updates

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-import "react-native-gesture-handler"
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { ThemeProvider, useTheme } from "@/contexts/ThemeContext";
 import { useAuthStore } from "@/state/authStore";
