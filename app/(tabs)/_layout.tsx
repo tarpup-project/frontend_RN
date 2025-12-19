507ca4ac1c77f6ba9b9f64b635d50ec545e2f100
Henrichy <henrynzekwe25@gmail.com>
2025-12-19 06:21:21 -0800

    Optimized chats

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -59,19 +59,7 @@ export default function TabLayout() {
             )
           }}
         />
-        <Tabs.Screen
-        name="tarps"
-        options={{
-          title: "Tarps",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "location" : "location-outline"}
-              size={28}
-              color={color}
-            />
-          )
-        }}
-      />
+      
 
 <Tabs.Screen
   name="groups"
