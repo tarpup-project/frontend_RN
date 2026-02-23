40d3b79948061e71368da7877364227383c4379c
Henrichy <henrynzekwe25@gmail.com>
2026-02-23 16:36:56 +0100

    BaseUrl change

--- constants/apiUrls.ts
+++ constants/apiUrls.ts
@@ -1,6 +1,6 @@
 class UrlConstants {
-  static readonly baseUrl = "http://172.20.10.3:4500";
-//http://server.tarpup.com"
+  static readonly baseUrl = "https://server.tarpup.com";
+//http://172.20.10.3:4500"
   static createUser = `/user/create`;
   static loginUser = `/user/login`;
   static verifyOTP = `/user/verify`;
